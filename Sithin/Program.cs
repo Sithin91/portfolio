f90b3812ae3878b75361239d5c28385809b8ffd4
Sithin91 <37984917+Sithin91@users.noreply.github.com>
2025-11-16 00:03:32 +0530

    https fixed

--- Sithin/Program.cs
+++ Sithin/Program.cs
@@ -12,16 +12,9 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-else
-{
-    // Only use HTTPS redirection in development
-    // Azure App Service handles HTTPS termination at the load balancer level
-    app.UseHttpsRedirection();
 }
 
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
