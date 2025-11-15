1f38cfe57f8ef20d5cb08e35e2b6eeb1bdf97fbf
Sithin91 <37984917+Sithin91@users.noreply.github.com>
2025-11-16 00:43:13 +0530

    Fix stdout logging path for Azure

--- Sithin/Program.cs
+++ Sithin/Program.cs
@@ -10,12 +10,10 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
-
 app.Run();
